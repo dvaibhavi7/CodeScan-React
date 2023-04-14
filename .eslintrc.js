812a5b5cf8bcd5d9de1a0363b9418573d86283ae
dvaibhavi7 <127049014+dvaibhavi7@users.noreply.github.com>
2023-04-14 12:21:36 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
       }
     },
     plugins: [
-        './node_modules/eslint-plugin-example-plugin'
+        'example-plugin'
     ],
     rules: {
       'no-unused-vars': ['warn', { 'args': 'none' }],
