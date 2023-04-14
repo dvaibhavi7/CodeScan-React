6a37e58d333deac917dc9f5f8ad89c8b95f0b8a9
dvaibhavi7 <127049014+dvaibhavi7@users.noreply.github.com>
2023-04-14 12:36:10 -0400

    Add files via upload

---
+++ eslint-plugin-example-plugin/index.js
@@ -0,0 +1,28 @@
+console.log('example-rule.js loaded');
+module.exports = {
+  rules: {
+    'example-rule': {
+      meta: {
+        docs: {
+          description: 'Scan for the word "vhd_api_key"',
+          category: 'Best Practices',
+          recommended: false,
+        },
+        fixable: null,
+        schema: [],
+      },
+      create: function (context) {
+        return {
+          Literal(node) {
+            if (typeof node.value === 'string' && node.value.includes('vhd_api_key')) {
+              context.report({
+                node,
+                message: 'Avoid using the word "vhd_api_key"',
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+};
